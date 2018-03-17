1afe3657bb56e50a07b3170234ebfb194f6c11c7
CindyTsai <hi993114@gmail.com>
2018-03-17 20:46:42 +0800

    Add file Assignment1-1.py

---
+++ Assignment/Assignment1-1.py
@@ -0,0 +1,20 @@
+import math
+import scipy.misc as misc
+
+
+def func(x):
+    value = 1.
+    ### START YOUR CODE HERE ###
+    for n in range(1, 5):
+        p = (float(n) * math.pi * x) / 2.
+        value = value * ((math.cos(p) * math.cosh(x)) + (math.sin(p) * math.sinh(x)))
+    #### END YOUR CODE HERE ####
+    return value
+
+
+def func_first_derivative(x):
+    value = 1.
+    ### START YOUR CODE HERE ###
+    value = misc.derivative(func, x, dx=10**-5)
+    #### END YOUR CODE HERE ####
+    return value
