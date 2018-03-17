74bed84c8c1801aaee99081881db10729e2bd078
CindyTsai <hi993114@gmail.com>
2018-03-17 21:13:08 +0800

    Add file Assignment1-2.py

---
+++ Assignment/Assignment1-2.py
@@ -0,0 +1,20 @@
+import math
+import scipy.integrate as integrate
+
+
+def func(x):
+    value = 0.
+    ### START YOUR CODE HERE ###
+    for n in range(1, 6):
+        p = float(n) * math.pi * x
+        value = value + (1 + (math.sin(p) * x) + (math.cos(p) * x**2))
+    #### END YOUR CODE HERE ####
+    return value
+
+
+def func_integrated(x):
+    value = 0.
+    ### START YOUR CODE HERE ###
+    (value, error) = integrate.quad(func, -x, x)
+    #### END YOUR CODE HERE ####
+    return value
