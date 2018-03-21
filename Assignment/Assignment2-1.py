e6a1407939856464818850ee90a1fdfe02a0b56f
CindyTsai <hi993114@gmail.com>
2018-03-21 15:51:06 +0800

    Add file Assignment2-1.py

---
+++ Assignment/Assignment2-1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def give_me_an_array(n):
+    output = np.zeros((10,10), dtype='int64')
+    ### START YOUR CODE HERE ###
+    output = output + np.fromfunction(lambda i,j: i-j == 1, (10,10))
+    output = output + np.fromfunction(lambda i,j: i-j == -1, (10,10))
+    output = output + np.fromfunction(lambda i,j: i+j == 8, (10,10))
+    output = output + np.fromfunction(lambda i,j: i+j == 10, (10,10))
+    output = n * output
+    #### END YOUR CODE HERE ####
+    return output
+
