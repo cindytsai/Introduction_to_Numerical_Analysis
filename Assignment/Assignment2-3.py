f57b43d0647e1b978918abe05f183324bc2d19a2
CindyTsai <hi993114@gmail.com>
2018-03-22 16:49:30 +0800

    Fixed Assignment2-3.py

--- Assignment/Assignment2-3.py
+++ Assignment/Assignment2-3.py
@@ -26,27 +26,22 @@ def game_of_life(p):
     p = np.insert(p,19, 0, axis = 0)
     p = np.insert(p, 0, 0, axis = 1)
     p = np.insert(p,12, 0, axis = 1)
-    def check(i, j, doa):
-        num = 0
-        index = [[i-1,j-1],[i-1,j],[i-1,j+1],
-                 [  i,j-1],        [  i,j+1],
-                 [i+1,j-1],[i+1,j],[i+1,j+1]]
-        for s in range(8):
-            num = num + p[index[s][0],index[s][1]]
-        if doa == 1:
-            if num < 2:
-                return 0
-            if num == 2 or num == 3 :
-                return 1
-            if num > 3 :
-                return 0
-        if doa == 0 and num == 3:
-            return 1
-        return num
+    ori = np.zeros((20,13))
     for u in range(1,19):
         for v in range(1,12):
-            p[u,v] = check(u,v,p[u,v])             
-    p = p[1:19, 1:12]
+            index = 0
+            num = np.sum(p[u-1:u+2,v-1:v+2]) - p[u,v]
+            if p[u,v] == 1 and index == 0:
+                if num < 2 :
+                    (index, ori[u,v]) = (1,0)
+                if num == 2 or num == 3 :
+                    (index, ori[u,v]) = (1,1)
+                if num > 3 :
+                    (index, ori[u,v]) = (1,0)
+            if p[u,v] == 0 and index == 0:
+                if num == 3 :
+                    (index, ori[u,v]) = (1,1)
+    p = ori[1:19, 1:12]
     #### END YOUR CODE HERE ####
     return p
 
