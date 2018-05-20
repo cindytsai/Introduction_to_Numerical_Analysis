95ca5814b12f2061ab82c61edda7929a95178991
CindyTsai <hi993114@gmail.com>
2018-05-20 16:14:39 +0800

    Add file Assignment6-2.py

---
+++ Assignment/Assignment6-2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def gen_random_chords(N):
+    res = np.zeros(N)
+    ### START YOUR CODE HERE ###
+    a = 2 * np.pi * np.random.rand(N)
+    b = 2 * np.pi * np.random.rand(N)
+    ax, ay = np.cos(a), np.sin(a)
+    bx, by = np.cos(b), np.sin(b)
+    res = np.sqrt((bx-ax)**2 + (by-ay)**2)
+    #### END YOUR CODE HERE ####
+    return res
+
