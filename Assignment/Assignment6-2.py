6d110c88a9fd955c3ad5d0380e5a03acda3efbd5
CindyTsai <hi993114@gmail.com>
2018-05-20 17:51:15 +0800

    Fixed file Assignment6-2.py

--- Assignment/Assignment6-2.py
+++ Assignment/Assignment6-2.py
@@ -3,8 +3,8 @@ import numpy as np
 def gen_random_chords(N):
     res = np.zeros(N)
     ### START YOUR CODE HERE ###
-    a = 2 * np.pi * np.random.rand(N)
-    b = 2 * np.pi * np.random.rand(N)
+    a = 2. * np.pi * np.random.rand(N)
+    b = 2. * np.pi * np.random.rand(N)
     ax, ay = np.cos(a), np.sin(a)
     bx, by = np.cos(b), np.sin(b)
     res = np.sqrt((bx-ax)**2 + (by-ay)**2)
